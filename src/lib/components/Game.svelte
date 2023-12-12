af3687b3309aa71851b5af3d3793e6f747c3a5ec
theneddlesking <71749344+theneddlesking@users.noreply.github.com>
2023-12-12 12:37:23 +1100

    Fix Original Coloring

--- src/lib/components/Game.svelte
+++ src/lib/components/Game.svelte
@@ -1,7 +1,19 @@
 <script lang="ts">
   import { ORIGINAL } from "../original";
 
+  let guess = "";
+
   console.log(ORIGINAL);
 </script>
 
 <h1>Game</h1>
+
+<input type="text" bind:value={guess} />
+
+<button
+  on:click={() => {
+    console.log(guess);
+    const response = ORIGINAL.makeGuess(guess);
+    console.log(response);
+  }}>Make Guess</button
+>
--- src/lib/original.ts
+++ src/lib/original.ts
@@ -3,16 +3,33 @@ import Game from "./game";
 export const ORIGINAL = new Game("hello", (guess: string, target: string) => {
   const colors = [];
 
+  // track indices of correctly guessed letters in the target
+  const correctIndices = new Set();
+
+  // check for correct letters in the correct position (green)
   for (let i = 0; i < guess.length; i++) {
     if (guess[i] === target[i]) {
-      // correct letter in correct position
-      colors.push("green");
-    } else if (target.includes(guess[i])) {
-      // correct letter in wrong position
-      colors.push("yellow");
-    } else {
-      // incorrect letter
-      colors.push("gray");
+      colors[i] = "green";
+      correctIndices.add(i);
+    }
+  }
+
+  // check for correct letters in the wrong position (yellow)
+  for (let i = 0; i < guess.length; i++) {
+    if (
+      guess[i] !== target[i] &&
+      target.includes(guess[i]) &&
+      !correctIndices.has(i)
+    ) {
+      colors[i] = "yellow";
+      correctIndices.add(i);
+    }
+  }
+
+  // fill the remaining slots with incorrect (gray)
+  for (let i = 0; i < guess.length; i++) {
+    if (!correctIndices.has(i)) {
+      colors[i] = "gray";
     }
   }
 
